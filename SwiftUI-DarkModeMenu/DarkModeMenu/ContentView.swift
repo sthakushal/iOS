cbf28dbc30cd70c268afb06ef95e25a10fb3ac10
Kushal Shrestha <stha.kushal@gmail.com>
2020-08-01 20:01:43 +0545

    darkmode menu added

--- SwiftUI-DarkModeMenu/DarkModeMenu/ContentView.swift
+++ SwiftUI-DarkModeMenu/DarkModeMenu/ContentView.swift
@@ -31,14 +31,42 @@ struct Home: View {
             GeometryReader {_ in
                 
                 VStack{
+                    ZStack{
+                        HStack{
+                            Button(action: {
+                                withAnimation(.default){
+                                    self.showMode.toggle()
+                                }
+                            }){
+                                Image("menu")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                            }
+                            Spacer()
+                        }
+                        Text("Home")
+                    }
+                    .padding()
+                    .foregroundColor(.primary)
+                    .overlay(Rectangle().stroke(Color.primary.opacity(0.1), lineWidth: 1).shadow(radius: 3))
+                    }
+                    Spacer()
                     Text("Dark mode menu")
+                    Spacer()
                 }
              }
+        HStack{
             
             Menu(dark: self.$darkMode, show: self.$showMode)
+                .preferredColorScheme(self.darkMode ? .dark : .light)
+                .offset(x:self.showMode ? 0 : -UIScreen.main.bounds.width/1.5)
+            Spacer(minLength: 0)
+        }
+        
+        .background(Color.primary.opacity(self.showMode ?(self.darkMode ? 0.05 : 0.2): 0).edgesIgnoringSafeArea(.all))
         }
     }
-}
+
 
 
 struct Menu: View {
@@ -49,7 +77,9 @@ struct Menu: View {
         VStack{
             HStack{
                 Button(action: {
-                    
+                    withAnimation(.default){
+                        self.showMode.toggle()
+                    }
                 }){
                     Image("back")
                     .resizable()
@@ -77,6 +107,103 @@ struct Menu: View {
                     .font(.caption)
             }
             . padding(.top, 25)
+            HStack(spacing:22){
+                Image(systemName: "moon.fill ")
+                    .font(.title)
+                Text("Dark Mode")
+                Spacer()
+                Button(action:{
+                    self.darkMode.toggle()
+                    UIApplication.shared.windows.first?.rootViewController?.view.overrideUserInterfaceStyle = self.darkMode ? .dark: .light
+                }){
+                    Image("off")
+                        .font(.title)
+                        .rotationEffect(.init(degrees: self.darkMode ? 180 :  0))
+                }
+            }
+            .padding(.top, 25)
+            Group{
+                Button(action: {    }){
+                    HStack(spacing: 22){
+                        Image("story")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                            .foregroundColor(.yellow)
+                        Text("Story")
+                        Spacer()
+                    }
+                }
+                
+                .padding(.top, 25)
+                
+                
+                Button(action: {    }){
+                                   HStack(spacing: 22){
+                                       Image("chat")
+                                       .resizable()
+                                       .frame(width: 25, height: 28)
+                                           .foregroundColor(.pink)
+                                       Text("Chats")
+                                    Spacer()
+                                   }
+                               }
+                               
+                               .padding(.top, 25)
+                Divider()
+                    .padding(.top,25)
+                
+                Button(action: {    }){
+                                   HStack(spacing: 22){
+                                       Image("media")
+                                       .resizable()
+                                       .frame(width: 25, height: 25)
+                                           .foregroundColor(.purple)
+                                       Text("Media and photo")
+                                    Spacer()
+                                   }
+                               }
+                               
+                               .padding(.top, 25)
+                
+                Button(action: {    }){
+                    HStack(spacing: 22){
+                        Image("setting")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                            .foregroundColor(.green)
+                        Text("Setting and Privacy")
+                     Spacer()
+                    }
+                }
+                
+                .padding(.top, 25)
+                
+                Button(action: {    }){
+                    HStack(spacing: 22){
+                        Image("help")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                            .foregroundColor(.blue)
+                        Text("Help Center")
+                     Spacer()
+                    }
+                }
+                
+                .padding(.top, 25)
+                
+                Button(action: {    }){
+                    HStack(spacing: 22){
+                        Image("notification")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                            .foregroundColor(.red)
+                        Text("Notification")
+                     Spacer()
+                    }
+                }
+                
+                .padding(.top, 25)
+            }
             Spacer()
         }
         .foregroundColor(.primary)
